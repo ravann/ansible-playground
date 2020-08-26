2584b6c5898f365fe282f936daee427ceab17cfa
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2020-08-26 17:28:20 +0800

    Cleaning kubernetes pods ...

--- build_docker.sh
+++ build_docker.sh
@@ -6,6 +6,10 @@ docker rmi ansible-server:latest
 docker rmi ravann/ansible-server:latest
 docker build --rm -f Dockerfile -t ansible-server:latest .
 
+# Cleanup any kubernetes
+ps -ef | grep kubectl | grep -v grep | cut -f 2 -d " " | xargs kill
+kubectl delete pod/ansible-server
+
 docker tag ansible-server:latest ravann/ansible-server:latest
 docker push ravann/ansible-server:latest
 
