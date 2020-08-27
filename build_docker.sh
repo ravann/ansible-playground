6c99a319f2fb152a9edd8cbd1e4af369ed781b6f
Ravan Nannapaneni <ravan.nannapaneni@oliverwyman.com>
2020-08-27 15:53:46 +0800

    Cleaned up the script ...

--- build_docker.sh
+++ build_docker.sh
@@ -3,14 +3,14 @@ cd ansible-server
 docker stop ansible-test
 docker rm ansible-test
 docker rmi ansible-server:latest
-docker rmi ravann/ansible-server:latest
-docker build --rm -f Dockerfile -t ansible-server:latest .
 
 # Cleanup any kubernetes
-ps -ef | grep kubectl | grep -v grep | cut -f 2 -d " " | xargs kill
 kubectl delete pod/ansible-server
 
-docker tag ansible-server:latest ravann/ansible-server:latest
-docker push ravann/ansible-server:latest
+docker build --rm -f Dockerfile -t ansible-server:latest .
+
+# Setup build on docker hub, dont have to push the image to docker hub
+# docker tag ansible-server:latest ravann/ansible-server:latest
+# docker push ravann/ansible-server:latest
 
 cd ..
